a03616655430579a6f7c5fd8bcf180431f3700ef
Lương Thị Phượng <yirpng@gmail.com>
2025-10-11 22:14:53 +0700

    update wishlist

--- src/components/wishlist/Panigation.jsx
+++ src/components/wishlist/Panigation.jsx
@@ -22,11 +22,6 @@ const PaginationComponent = ({
                 onChange={onPageChange}
                 showSizeChanger={false}
                 showQuickJumper={false}
-                showTotal={(total, range) => (
-                    <span className="text-gray-600 text-sm">
-                        {`${range[0]}-${range[1]} trong ${total} sản phẩm`}
-                    </span>
-                )}
                 locale={{
                     items_per_page: '/ trang',
                     jump_to: 'Đến',
