87da178726a1b57fbfc34a807d15214cd8b033f5
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2025-10-11 22:15:18 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ const App = () => {
     const role = getCookie("role");
 
     if (!role) {
-      return <Navigate to="/login" replace />;
+      return <Navigate to="/auth" replace />;
     }
 
     if (!allowedRoles.includes(role)) {
